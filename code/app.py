14a7fa7d2ca364c2cbb8d04e3db0bd5e5c5418bd
Ajitesh Rajput <rajput.ajitesh@gmail.com>
2025-10-28 22:13:02 +0530

    gemini versionUpdate app.py

--- code/app.py
+++ code/app.py
@@ -5,7 +5,7 @@ from docx import Document
 
 #Setup API
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model = genai.GenerativeModel("models/gemini-1.5-flash")
+model = genai.GenerativeModel("models/gemini-1.5-pro-latest")
 
 #Extracting text from pdf
 def extract_text_from_pdf(uploaded_file):
